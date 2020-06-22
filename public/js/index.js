ec6aee79c8c6f7fb00b9ddffed7cfdc2c2ecc619
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2020-06-22 20:04:11 -0300

    bugfix http para https

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
 const BASE_URL_APP = 'https://veg-me.herokuapp.com';
 
+if (window.location.host == 'http://veg-me.herokuapp.com') {
+  window.location.href = BASE_URL_APP
+}
+
 $("#btn-footer-amigos").click(function (event) {
 
   $('#feed').addClass('d-none');
